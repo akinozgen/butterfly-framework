f3059d33c602cbcab89ca18d9a9e66121bc61ba5
Akın Özgen <akinozgen17@outlook.com>
2017-06-04 15:17:29 +0300

    installed twig, created activeclass

---
+++ core/System/ActiveClass.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: akinozgen
+ * Date: 6/4/17
+ * Time: 3:10 PM
+ */
+
+namespace Butterfly\System;
+
+/**
+ * Class ActiveClass
+ * @package Butterfly\System
+ */
+class ActiveClass
+{
+
+
+
+}
