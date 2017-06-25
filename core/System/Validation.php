e1aa05d148b83bfbecaa43d11ceb524920dad827
Akın Özgen <akinozgen17@outlook.com>
2017-06-25 18:12:47 +0300

    Validation class added

--- core/System/Validation.php
+++ core/System/Validation.php
@@ -19,45 +19,71 @@ class Validation
         $this->result = [];
     }
 
+    /**
+     * @param $pair1
+     * @param $pair2
+     * @param string $label
+     * @return $this
+     */
     public function validate_pairs($pair1, $pair2, $label = 'pairs') {
         if ($pair2 === $pair1) {
             $this->check = true;
-            $this->result[$label] = 'passed';
         }
         else {
             $this->check = false;
-            $this->result[$label] = 'failed';
+            $this->result[] = $label;
         }
 
         return $this;
     }
 
+    /**
+     * @param $email
+     * @param string $label
+     * @return $this
+     */
     public function validate_email($email, $label = 'email') {
         //TODO: email validation condition
         if (true) {
             $this->check = true;
-            $this->result[$label] = 'passed';
         } else {
             $this->check  = false;
-            $this->result[$label] = 'failed';
+            $this->result[] = $label;
         }
 
         return $this;
     }
 
+    /**
+     * @param $str
+     * @param $min
+     * @param $max
+     * @param string $label
+     * @return $this
+     */
     public function validate_length($str, $min, $max, $label = 'length') {
         if (strlen($str) <= $max && strlen($str) >= $min) {
             $this->check = true;
             $this->result[$label] = 'passed';
         } else {
             $this->check = false;
-            $this->result[$label] = 'failed';
+            $this->result[] = $label;
         }
 
         return $this;
     }
 
-    public function get_result() {
+    /**
+     * @return array
+     */
+    public function get_results() {
         return $this->result;
+    }
+
+    /**
+     * @return bool
+     */
+    public function get_result() {
+        return $this->check;
     }
 }
